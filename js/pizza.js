0eda66ab2b5c56893f927e49c37ed9e1db906b4e
jakes2020 <19jacris@waipahuh.k12.hi.us>
2018-02-07 14:43:14 -1000

    2

--- js/pizza.js
+++ js/pizza.js
@@ -5,6 +5,70 @@
 * Keys with more than one value should be an array of strings ex. Veggies
 */
 
+var Customers = {
+
+		order11821:{
+			CustomerName: "James Franko",
+			Address: "2155 E Lake Dr",
+			City: "Seattle",
+			State: "WA",
+			ZipCode: 11310,
+				Pizza:{
+				Crust: "thin",
+  				PizzaSauce: "robust marinara",
+  				Cheese: "regular",
+ 				Toppings:
+    			Veggies: [
+    				"olives", "green peppers"
+    				],
+    			Meats: [
+    				"pepperoni", "ham", "sausage"
+    				],
+				SideItems: [
+				"breadsticks", "bbq wings" "6 piece"
+				],
+				Sauces: "ranch"
+				}
+			Special Instructions: "please cut into squares"
+			Payment Type: "cash"
+			Store Id: 112
+	}
+		order11372: {
+			CustomerName: "Justin Bieber",
+			Address: "1801 W Rodeo Dr",
+			City: "Beverly Hills",
+			State: "CA",
+			ZipCode: 91833,
+				Pizza:{
+
+					Crust: "thin",
+  					PizzaSauce: "creamy garlic alfredo",
+  					Crust: "stuffed crust",
+  					Cheese: "double",
+ 					Toppings:{
+    				Veggies: [
+    					"olives", "green peppers"
+    					],
+    				Meats: [
+    					"pepperoni", "ham", "sausage"
+    					]
+    				}
+					SideItems: [
+					"double fudge brownie dessert"
+						],
+					Sauces: "bbq", "garlic sauce"
+						}
+				Special Instructions: "call 815 at the gate"
+				Payment Type: "card"
+				Store Id: 382
+	}
+
+
+}
+var currentOrders = {
+	1:order11372,
+	2:order11821
+} 
 
 /*
 * Create a new variable named currentOrders
